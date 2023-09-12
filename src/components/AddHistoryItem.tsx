974abf9b61cb5f59d656fc6b9cbad74186c98b97
Hasnain Nwl <hasnainnwl@gmil.com>
2023-09-12 11:47:17 +0500

    Heeloo Ehsas Lab

--- src/components/AddHistoryItem.tsx
+++ src/components/AddHistoryItem.tsx
@@ -3,12 +3,12 @@ import { title } from "process";
 const AddHistoryItem = ({transtion}:any) =>{
     const {title,amount} = transtion;
     return(
-        <ul id="list" className="list">
+        
             
         <li className= {amount < 0 ? "minus" : "plus"}>
           {title} <span>{amount}</span><button className="delete-btn">x</button>
         </li> 
-      </ul>
+      
 
     );
 }
